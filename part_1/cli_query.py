3d2f0de9b7b0907809dee2ebfd31ab0d3404c1f8
Dmytro-Tarasenko <ug.dotenko@gmail.com>
2024-03-15 16:36:42 +0200

    update

--- part_1/cli_query.py
+++ part_1/cli_query.py
@@ -3,4 +3,9 @@ from redis import Redis
 from mongoengine import connect
 
 from odms import Quote
-from models import QuoteModel, AuthorModel
+from models import QuoteModel
+
+connect('quotes')
+
+redis = Redis(host='redis', port=6379)
+
